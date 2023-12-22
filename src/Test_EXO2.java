9b6807c72780f6da861f79fbde8fc9938052cf6c
Hamza-Erradi <hamzaerradi433@gmail.com>
2023-12-22 11:43:43 +0100

    TD 2

---
+++ src/Test_EXO2.java
@@ -0,0 +1,10 @@
+public class Test_EXO2 {
+    public static void main(String []args){
+        EXO2_Point a = new  EXO2_Point();
+        a.setNom('A');
+        a.setX(7);
+        a.setY(2);
+        System.out.println(a);
+
+    }
+}
